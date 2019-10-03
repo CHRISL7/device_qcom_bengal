ba782a6b1155a8fffc489e6efdef850362edcb9d
Mohit Sharma <smohit@codeaurora.org>
2019-10-03 23:55:41 +0530

    bengal: Enable compilation of kernel tests modules.
    TARGET_USES_QMAA_OVERRIDE_KERNEL_TESTS_INTERNAL set to true for
    enabling kernel internal test modules compilations.
    
    Change-Id: Ie50974741119ae0c9716deec70b5f744c2eb04c9

--- bengal.mk
+++ bengal.mk
@@ -79,6 +79,8 @@ TARGET_USES_QMAA := true
 #true means overriding global QMAA for this tech area
 #false means using global, no override
 
+TARGET_USES_QMAA_OVERRIDE_KERNEL_TESTS_INTERNAL := true
+
 #TARGET_USES_QMAA_OVERRIDE_DISPLAY := true
 #TARGET_USES_QMAA_OVERRIDE_AUDIO   := true
 #TARGET_USES_QMAA_OVERRIDE_VIDEO   := true
