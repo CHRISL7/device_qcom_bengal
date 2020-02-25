7bc69c8cae73aa09a20930384ee04afc4814163f
Shadab Naseem <snaseem@codeaurora.org>
2020-02-25 14:19:58 +0530

    BoardConfig: Add support for header version 2 of Bengal boot image
    Adding support to make a bootimage with header version as 2.
    This bootimage no longer has the dtbs appended to the kernel.
    All the dtbs are made into a new image called dtb.img and
    this image is present in the bootimage.
    
    Change-Id: I611bd69762b23a0a3cc7ed1334df9e92f7d1c7cc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,9 +34,10 @@ BOARD_PRESIL_BUILD := true
 
 USE_OPENGL_RENDERER := true
 
-
+#Enable dtb in boot image and boot image header version 2 support.
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 # Set Header version for bootimage
-BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 ifeq ($(ENABLE_AB), true)
@@ -149,7 +150,8 @@ TARGET_NO_RPC := true
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 TARGET_INIT_VENDOR_LIB := libinit_msm
 
-TARGET_KERNEL_APPEND_DTB := true
+#Disable appended dtb.
+TARGET_KERNEL_APPEND_DTB := false
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
 #Enable PD locater/notifier
