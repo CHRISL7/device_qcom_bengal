a35471397e7a989ab7a8ec10a0f3aba9758eb792
Santosh Mardi <gsantosh@codeaurora.org>
2020-05-26 08:56:43 +0530

    bengal: move memlat settings to separate script
    In Latest android version there are kernel security hooks
    added in kernel restricting any userspace process or thread
    to open perf events, this change needs elevated selinux
    permission for perf activity.
    
    Move the settings to a new script and give selinux permission
    for perf activity for this new script.
    
    Change-Id: I7843a3f67968906072fab4ddeda7b84b7aded989

--- bengal.mk
+++ bengal.mk
@@ -233,6 +233,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.crypto.volume.filenames_mode = "aes-256-cts" \
 ro.crypto.allow_encrypt_override = true
 
+PRODUCT_PACKAGES += init.qti.dcvs.sh
+
 #----------------------------------------------------------------------
 # wlan specific
 #----------------------------------------------------------------------
