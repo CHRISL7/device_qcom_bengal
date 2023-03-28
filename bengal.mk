c3094cdbd62eaa9e679cca7482cbe96f1485ce30
KUIpru <rakeshtiwari349@gmail.com>
2023-03-28 17:30:19 +0000

    bengal: 515 diff
    Signed-off-by: KUIpru <rakeshtiwari349@gmail.com>

--- bengal.mk
+++ bengal.mk
@@ -1,12 +1,19 @@
+TARGET_BOARD_PLATFORM := bengal
+TARGET_BOARD_SUFFIX := _515
+
+ALLOW_MISSING_DEPENDENCIES := true
+RELAX_USES_LIBRARY_CHECK := true
+
 # Enable AVB 2.0
 BOARD_AVB_ENABLE := true
 
 # Enable Virtual A/B
 ENABLE_VIRTUAL_AB := true
 
-ifeq ($(ENABLE_VIRTUAL_AB), true)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-endif
+# Enable virtual A/B compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/android_t_baseline.mk)
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := gz
 
 # Default A/B configuration
 ENABLE_AB ?= true
@@ -16,27 +23,38 @@ SYSTEMEXT_SEPARATE_PARTITION_ENABLE = true
 # Enable Dynamic partition
 BOARD_DYNAMIC_PARTITION_ENABLE ?= true
 
-SHIPPING_API_LEVEL := 30
+# true: earlycon and console enabled
+# false: console explicitly disabled
+# <empty>: default from kernel
+TARGET_CONSOLE_ENABLED ?=
+
+SHIPPING_API_LEVEL := 33
 PRODUCT_SHIPPING_API_LEVEL := $(SHIPPING_API_LEVEL)
 
-BOARD_SHIPPING_API_LEVEL := 30
-BOARD_API_LEVEL := 30
+# Set GRF/Vendor freeze properties
+BOARD_SHIPPING_API_LEVEL := 33
+BOARD_API_LEVEL := 33
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # For QSSI builds, we should skip building the system image. Instead we build the
 # "non-system" images (that we support).
 
 PRODUCT_BUILD_SYSTEM_IMAGE := false
 PRODUCT_BUILD_SYSTEM_OTHER_IMAGE := false
+PRODUCT_BUILD_SYSTEM_DLKM_IMAGE := true
 PRODUCT_BUILD_VENDOR_IMAGE := true
+PRODUCT_BUILD_VENDOR_DLKM_IMAGE := true
 PRODUCT_BUILD_PRODUCT_IMAGE := false
 PRODUCT_BUILD_PRODUCT_SERVICES_IMAGE := false
-PRODUCT_BUILD_ODM_IMAGE := false
+PRODUCT_BUILD_ODM_IMAGE := true
 ifeq ($(ENABLE_AB), true)
 PRODUCT_BUILD_CACHE_IMAGE := false
 else
 PRODUCT_BUILD_CACHE_IMAGE := true
 endif
 PRODUCT_BUILD_RAMDISK_IMAGE := true
+PRODUCT_BUILD_RECOVERY_IMAGE := true
 PRODUCT_BUILD_USERDATA_IMAGE := true
 
 TARGET_SKIP_OTA_PACKAGE := true
@@ -56,7 +74,11 @@ else
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += fastbootd
 # Add default implementation of fastboot HAL.
-PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
+PRODUCT_PACKAGES += android.hardware.fastboot@1.1-impl-mock
+
+# diag-router
+TARGET_HAS_DIAG_ROUTER := true
+
 # f2fs utilities
 PRODUCT_PACKAGES += \
  sg_write_buffer \
@@ -75,23 +97,26 @@ POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
 FILESYSTEM_TYPE_vendor=ext4 \
 POSTINSTALL_OPTIONAL_vendor=true
 # Userdata checkpoint end
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.emmc
 else
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_non_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_non_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_non_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/first_stage_ramdisk/fstab.default
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_non_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/first_stage_ramdisk/fstab.emmc
 endif
 BOARD_AVB_VBMETA_SYSTEM := system
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
-$(call inherit-product, build/make/target/product/gsi_keys.mk)
+endif
+
+ifneq ("$(wildcard device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/vendor_dlkm/system_dlkm.modules.blocklist)", "")
+PRODUCT_COPY_FILES += device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/vendor_dlkm/system_dlkm.modules.blocklist:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
 endif
 
 BOARD_HAVE_BLUETOOTH := false
 BOARD_HAVE_QCOM_FM := false
-TARGET_DISABLE_PERF_OPTIMIATIONS := false
+TARGET_DISABLE_PERF_OPTIMIZATIONS := false
 
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
@@ -107,6 +132,7 @@ PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
 
 TARGET_DEFINES_DALVIK_HEAP := true
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/qcom/vendor-common/common64.mk)
 # Temporary bring-up config <--
 
@@ -122,8 +148,8 @@ PRODUCT_PROPERTY_OVERRIDES  += \
      dalvik.vm.heapminfree=512k \
      dalvik.vm.heapmaxfree=8m
 # Target naming
-PRODUCT_NAME := bengal
-PRODUCT_DEVICE := bengal
+PRODUCT_NAME := bengal_515
+PRODUCT_DEVICE := bengal_515
 PRODUCT_BRAND := qti
 PRODUCT_MODEL := Bengal for arm64
 
@@ -132,17 +158,126 @@ TARGET_USES_AOSP := false
 TARGET_USES_AOSP_FOR_AUDIO := false
 TARGET_USES_QCOM_BSP := false
 
+# beluga settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.beluga.p=0x3 \
+    ro.vendor.beluga.c=0x4800 \
+    ro.vendor.beluga.s=0x900 \
+    ro.vendor.beluga.t=0x240
+
+# Below perf props should be part of vendor/build.prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.perf-hal.ver=2.3 \
+    ro.vendor.perf.scroll_opt=1 \
+    vendor.perf.framepacing.enable=1
+
 # RRO configuration
 TARGET_USES_RRO := true
 
 TARGET_DISABLE_DISPLAY := false
 
-# Kernel configurations
-TARGET_KERNEL_VERSION := 4.19
-#Enable llvm support for kernel
-KERNEL_LLVM_SUPPORT := true
-#Enable sd-llvm support for kernel
-KERNEL_SD_LLVM_SUPPORT := true
+NEED_AIDL_NDK_PLATFORM_BACKEND := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+
+###########
+#QMAA flags starts
+###########
+#QMAA global flag for modular architecture
+#true means QMAA is enabled for system
+#false means QMAA is disabled for system
+
+TARGET_USES_QMAA := true
+
+#QMAA flag which is set to incorporate any generic dependencies
+#required for the boot to UI flow in a QMAA enabled target.
+#Set to false when all target level depenencies are met with
+#actual full blown implementations.
+TARGET_USES_QMAA_RECOMMENDED_BOOT_CONFIG := true
+
+#QMAA tech team flag to override global QMAA per tech team
+#true means overriding global QMAA for this tech area
+#false means using global, no override
+TARGET_USES_QMAA_OVERRIDE_RPMB := true
+TARGET_USES_QMAA_OVERRIDE_SMCINVOKE := true
+TARGET_USES_QMAA_OVERRIDE_GPT := true
+TARGET_USES_QMAA_OVERRID_KMGK := true
+TARGET_USES_QMAA_OVERRIDE_DISPLAY := true
+# Set to true
+TARGET_USES_QMAA_OVERRIDE_AUDIO   := true
+TARGET_USES_QMAA_OVERRIDE_VIDEO   := true
+TARGET_USES_QMAA_OVERRIDE_CAMERA  := true
+TARGET_USES_QMAA_OVERRIDE_GFX     := true
+TARGET_USES_QMAA_OVERRIDE_WFD     := true
+TARGET_USES_QMAA_OVERRIDE_GPS     := true
+TARGET_USES_QMAA_OVERRIDE_ANDROID_RECOVERY := true
+TARGET_USES_QMAA_OVERRIDE_ANDROID_CORE := true
+TARGET_USES_QMAA_OVERRIDE_WLAN    := true
+TARGET_USES_QMAA_OVERRIDE_DPM  := false
+TARGET_USES_QMAA_OVERRIDE_BLUETOOTH   := true
+TARGET_USES_QMAA_OVERRIDE_FM  := true
+TARGET_USES_QMAA_OVERRIDE_CVP  := false
+TARGET_USES_QMAA_OVERRIDE_FASTCV  := true
+TARGET_USES_QMAA_OVERRIDE_FASTRPC := true
+TARGET_USES_QMAA_OVERRIDE_SCVE  := true
+TARGET_USES_QMAA_OVERRIDE_OPENVX  := true
+TARGET_USES_QMAA_OVERRIDE_DIAG := false
+TARGET_USES_QMAA_OVERRIDE_FTM := false
+TARGET_USES_QMAA_OVERRIDE_DATA := true
+TARGET_USES_QMAA_OVERRIDE_DATA_NET := true
+TARGET_USES_QMAA_OVERRIDE_KERNEL_TESTS_INTERNAL := true
+TARGET_USES_QMAA_OVERRIDE_MSMIRQBALANCE := true
+TARGET_USES_QMAA_OVERRIDE_VIBRATOR := false
+TARGET_USES_QMAA_OVERRIDE_DRM     := true
+TARGET_USES_QMAA_OVERRIDE_KMGK := true
+TARGET_USES_QMAA_OVERRIDE_VPP := false
+TARGET_USES_QMAA_OVERRIDE_GP := true
+TARGET_USES_QMAA_OVERRIDE_BIOMETRICS := true
+TARGET_USES_QMAA_OVERRIDE_SPCOM_UTEST := false
+TARGET_USES_QMAA_OVERRIDE_PERF := true
+TARGET_USES_QMAA_OVERRIDE_SENSORS := true
+TARGET_USES_QMAA_OVERRIDE_SYNX := false
+TARGET_USES_QMAA_OVERRIDE_SECUREMSM_TESTS := true
+TARGET_USES_QMAA_OVERRIDE_SOTER := true
+TARGET_USES_QMAA_OVERRIDE_REMOTE_EFS := false
+TARGET_USES_QMAA_OVERRIDE_TFTP := false
+TARGET_USES_QMAA_OVERRIDE_EID := false
+TARGET_USES_QMAA_OVERRIDE_USB := true
+
+TARGET_ENABLE_QSEECOM := true
+#Full QMAA HAL List
+QMAA_HAL_LIST := audio video camera display sensors gps
+
+ifeq ($(TARGET_USES_QMAA), true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.confqmaa=true
+endif
+
+###########
+#QMAA flags ends
+
+CLEAN_UP_JAVA_IN_VENDOR ?= enforcing
+
+JAVA_IN_VENDOR_SOONG_WHITE_LIST :=\
+CuttlefishService\
+pasrservice\
+VendorPrivAppPermissionTest\
+
+JAVA_IN_VENDOR_MAKE_WHITE_LIST :=\
+AEye\
+AON\
+SnapdragonCamera\
+
+# Set kernel version and ion flags
+TARGET_KERNEL_VERSION := 5.15
+TARGET_USES_NEW_ION := true
+
+# Disable DLKM generation until build support is available
+TARGET_KERNEL_DLKM_DISABLE := false
+
+#Suppot to compile recovery without msm headers
+TARGET_HAS_GENERIC_KERNEL_HEADERS := true
 
 ###########
 # Target configurations
@@ -171,9 +306,9 @@ ifeq ($(ENABLE_AB), true)
 PRODUCT_PACKAGES += update_engine \
     update_engine_client \
     update_verifier \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_HOST_PACKAGES += \
     brillo_update_payload
@@ -184,10 +319,13 @@ PRODUCT_PACKAGES += \
   update_engine_sideload
 
 endif
-DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/bengal/framework_manifest.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/bengal_515/framework_manifest.xml
 
-DEVICE_MANIFEST_FILE := device/qcom/bengal/manifest.xml
+DEVICE_MANIFEST_FILE := device/qcom/bengal_515/manifest.xml
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
+
+# Enable compilation of image_generation_tool
+TARGET_USES_IMAGE_GEN_TOOL := true
 
 # Kernel modules install path
 KERNEL_MODULES_INSTALL := dlkm
@@ -205,8 +343,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
-# Audio configuration file
--include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/bengal/bengal.mk
 
 # MIDI feature
 PRODUCT_COPY_FILES += \
@@ -216,13 +352,48 @@ PRODUCT_COPY_FILES += \
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
-BOARD_VNDK_VERSION := current
+BOARD_SYSTEMSDK_VERSIONS := 33
+
+ifeq (true,$(BUILDING_WITH_VSDK))
+    ALLOW_MISSING_DEPENDENCIES := true
+    TARGET_SKIP_CURRENT_VNDK := true
+
+    ifneq (,$(filter vendor,$(DISABLED_VSDK_SNAPSHOTS_LIST)))
+        # Vendor snapshot is disabled with VSDK
+        BOARD_VNDK_VERSION := current
+    else
+        BOARD_VNDK_VERSION := 33
+    endif
+
+    ifneq (,$(filter recovery,$(DISABLED_VSDK_SNAPSHOTS_LIST)))
+        # Recovery snapshot is disabled with VSDK
+        RECOVERY_SNAPSHOT_VERSION := current
+    else
+        RECOVERY_SNAPSHOT_VERSION := 33
+    endif
+
+    ifneq (,$(filter ramdisk,$(DISABLED_VSDK_SNAPSHOTS_LIST)))
+        # Ramdisk snapshot is disabled with VSDK
+        RAMDISK_SNAPSHOT_VERSION := current
+    else
+        RAMDISK_SNAPSHOT_VERSION := 33
+    endif
+else
+    BOARD_VNDK_VERSION := current
+    RECOVERY_SNAPSHOT_VERSION := current
+    RAMDISK_SNAPSHOT_VERSION := current
+endif
+
+$(warning "BOARD_VNDK_VERSION = $(BOARD_VNDK_VERSION), RECOVERY_SNAPSHOT_VERSION=$(RECOVERY_SNAPSHOT_VERSION), RAMDISK_SNAPSHOT_VERSION=$(RAMDISK_SNAPSHOT_VERSION)")
+
 TARGET_MOUNT_POINTS_SYMLINKS := false
 
 PRODUCT_BOOT_JARS += telephony-ext
 PRODUCT_PACKAGES += telephony-ext
 
 PRODUCT_BOOT_JARS += tcmiface
+
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
 
 # Vendor property to enable advanced network scanning
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -237,12 +408,30 @@ ro.crypto.volume.filenames_mode = "aes-256-cts" \
 ro.crypto.allow_encrypt_override = true
 
 PRODUCT_PACKAGES += init.qti.dcvs.sh
-PRODUCT_PACKAGES += android.hardware.lights-service.qti
+PRODUCT_PACKAGES += android.hardware.sensors@2.1-service.multihal
+PRODUCT_PACKAGES += android.hardware.sensors@2.1-service-multihal.rc
+# Enable legacy QSEECom
+TARGET_ENABLE_QSEECOM := true
 
 #----------------------------------------------------------------------
 # wlan specific
 #----------------------------------------------------------------------
+ifeq ($(TARGET_USES_QMAA), true)
+ifneq ($(TARGET_USES_QMAA_OVERRIDE_WLAN), true)
+include device/qcom/wlan/default/wlan.mk
+else
 include device/qcom/wlan/bengal/wlan.mk
+endif
+else
+include device/qcom/wlan/bengal/wlan.mk
+endif
+
+#------------------------
+# location specific
+#------------------------
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+FEATURE_SLIM_AP := false
+FEATURE_GPS_LOC_QSH := false
 
 ###################################################################################
 # This is the End of target.mk file.
